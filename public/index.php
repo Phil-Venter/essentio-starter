19b20a9e3d35ac79a77b1bf295611d37118f331a
Philip <mrphilventer@gmail.com>
2025-07-02 09:33:54 +0200

    update core

--- public/index.php
+++ public/index.php
@@ -1,12 +1,11 @@
 <?php
-use Essentio\Core\Application;
 
 require_once __DIR__ . "/../vendor/autoload.php";
-Application::http(__DIR__ . "/..");
+Essentio\Core\Application::http(__DIR__ . "/..");
 require_once base_path("bootstrap.php");
 
 get("", [Action\Home::class, "view"]);
 post("", [Action\Home::class, "nameAction"]);
 get(":name", [Action\Home::class, "nameView"]);
 
-Application::run();
+Essentio\Core\Application::run();
